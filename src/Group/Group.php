c325410c015e7c2a590d91b97ec834603ad4156a
mayunf <yunfeng0614@qq.com>
2020-01-10 02:06:45 +0000

    Apply fixes from StyleCI

--- src/Group/Group.php
+++ src/Group/Group.php
@@ -1,25 +1,21 @@
 <?php
 
-
 namespace InsideAPI\Group;
-
 
 use InsideAPI\Core\AbstractAPI;
 
 class Group extends AbstractAPI
 {
-
     const USER_INVITE_INFO = 'ins/v2/group/GroupUserInviteInfo';
 
     const USER_INVITE_ADD = 'ins/v2/group/GroupUserInviteAdd';
-
 
     public function userInviteInfo($inviteId)
     {
         return $this->parseJSON(static::POST, [
             self::USER_INVITE_INFO,
             [
-                'InviteId'   => $inviteId
+                'InviteId'   => $inviteId,
             ],
         ]);
     }
@@ -29,19 +25,19 @@ class Group extends AbstractAPI
      * @param $code
      * @param string $m
      * @param string $un
+     *
      * @return \Mayunfeng\Supports\Collection
      */
-    public function userInviteAdd($inviteId,$code,$m='',$un='')
+    public function userInviteAdd($inviteId, $code, $m = '', $un = '')
     {
         return $this->parseJSON(static::POST, [
             self::USER_INVITE_ADD,
             [
                 'InviteId'   => $inviteId,
-                'UName'  => $un,
-                'Mobile'    => $m,
-                'Code'     => $code
+                'UName'      => $un,
+                'Mobile'     => $m,
+                'Code'       => $code,
             ],
         ]);
     }
-
 }
